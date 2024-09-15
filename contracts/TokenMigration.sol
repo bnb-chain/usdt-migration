c746f120af806753c90f01748738e24e4cb6f015
Ethan <cosinlinker@gmail.com>
2024-09-15 15:34:00 +0800

    feat: init TokenMigration contract

---
+++ contracts/TokenMigration.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+contract TokenMigration is Ownable2Step {
+
+}
